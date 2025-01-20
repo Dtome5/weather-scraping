64b0c2375c5174b62359581cef40a072cecad2bb
Dtome5 <azogudaniel00@gmail.com>
2025-01-20 12:04:19 +0100

    20-1-2025

--- extract.py
+++ extract.py
@@ -5,6 +5,9 @@ import re
 import json
 import urllib.request
 import numpy as np
+import openmeteo_requests
+import requests_cache
+from retry_requests import retry
 
 from datetime import datetime, timedelta
 from bs4 import BeautifulSoup as bs
@@ -20,13 +23,11 @@ def to_num(var: str):
 
 
 def get_temp(soup: bs):
-    # val = soup.find("div", {"class", "current-temp"})
     val = soup.select_one(".CurrentConditions--tempValue--zUBSz")
     return val.text
 
 
 def get_humidity(soup: bs):
-    # val = soup.find("lib-display-unit", {"type": "humidity"})
     val = soup.select_one(
         "div.WeatherDetailsListItem--WeatherDetailsListItem--HLP3I:nth-child(3) > div:nth-child(3) > span:nth-child(1)"
     )
@@ -34,36 +35,62 @@ def get_humidity(soup: bs):
 
 
 def get_loc(soup: bs):
-    # val = soup.find("strong", {"class": "location-data"})
     val = soup.select_one(".CurrentConditions--location--yub4l")
     return val.text
 
 
 def get_wind_speed(soup: bs):
     val = soup.select_one(".Wind--windWrapper--NsCjc > span:nth-child(2)")
-    # val = soup.find("header", {"class": "wind-speed"})
     return val.text
 
 
-def append_hdf5(group, dataset, obj):
-    data = np.array([obj], dtype="i")
-    if dataset not in group:
-        group.create_dataset(dataset, data=data, maxshape=(None,), chunks=True)
-    else:
-        group[dataset].resize((group[dataset].shape[0] + len(data),))
-        group[dataset][-len(data) :] = data
+# """
+def meteo(loc, lat, long):
+    cache_session = requests_cache.CachedSession(".cache", expire_after=-1)
+    retry_session = retry(cache_session, retries=5, backoff_factor=0.2)
+    openmeteo = openmeteo_requests.Client(session=retry_session)
+    url = "https://archive-api.open-meteo.com/v1/archive"
+    end_date = datetime.now()
+    start_date = end_date - timedelta(days=14)
+    params = {
+        "latitude": lat,
+        "longitude": long,
+        "start_date": start_date.strftime("%Y-%m-%d"),
+        "end_date": end_date.strftime("%Y-%m-%d"),
+        "hourly": ["temperature_2m", "relative_humidity_2m", "wind_speed_10m"],
+        "temporal_resolution": "hourly_6",
+    }
+
+    responses = openmeteo.weather_api(url, params=params)
+    response = responses[0]
+
+    hourly = response.Hourly()
+    hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
+    hourly_relative_humidity_2m = hourly.Variables(1).ValuesAsNumpy()
+    hourly_wind_speed_10m = hourly.Variables(2).ValuesAsNumpy()
+
+    hourly_data = {
+        "date": pd.date_range(
+            start=pd.to_datetime(hourly.Time(), unit="s", utc=True),
+            end=pd.to_datetime(hourly.TimeEnd(), unit="s", utc=True),
+            freq=pd.Timedelta(seconds=hourly.Interval()),
+            inclusive="left",
+        )
+    }
+
+    hourly_data["temperature_2m"] = hourly_temperature_2m
+    hourly_data["relative_humidity_2m"] = hourly_relative_humidity_2m
+    hourly_data["wind_speed_10m"] = hourly_wind_speed_10m
+
+    hourly_dataframe = pd.DataFrame(data=hourly_data)
+    return hourly_dataframe
 
 
-"""
-website = "https://openweathermap.org/city/2953435"
-page = requests.get(website).content
-soup = bs(page, "html.parser")
-# print(soup)
-print(soup.find("span"))
-"""
+# """
+
 file = h5py.File("weather.hdf5", "a")
 websites = pd.read_csv("websites.csv")
-today = datetime.now().strftime("%y-%m-%d")
+today = datetime.now().strftime("%Y-%m-%d-%H-%M")
 urls = [websites["URL"][i] for i in range(len(websites))]
 pages = [requests.get(url) for url in urls]
 scraped_pages = [bs(page.content, "html.parser") for page in pages]
@@ -72,18 +99,19 @@ humidity = [get_humidity(scraped_page) for scraped_page in scraped_pages]
 temperature = [to_num(temp) for temp in temperature]
 humidity = [to_num(hum) for hum in humidity]
 locations = websites["Location"]
-# [get_loc(scraped_page) for scraped_page in scraped_pages]
 wind_speeds = [get_wind_speed(scraped_page) for scraped_page in scraped_pages]
 frames = {}
-for i in range(len(locations)):
-    frames[locations[i]] = pd.DataFrame(
-        {
-            "time": [today],
-            "temperature": [temperature[i]],
-            "humidity": [humidity[i]],
-            "wind_speed": [wind_speeds[i]],
-        }
-    )
+frame["London"] = hourly
+# for i in range(len(locations)):
+#     frames[locations[i]] = pd.DataFrame(
+#         {
+#             "time": [today],
+#             "temperature": [temperature[i]],
+#             "humidity": [humidity[i]],
+#             "wind_speed": [wind_speeds[i]],
+#         }
+#     )
 
 for frame in frames.keys():
-    frames[frame].to_hdf("weather3.hdf5", key=frame)
+    df = pd.read_hdf("weather.hdf5", key=frame)
+    frames[frame].to_hdf("weather.hdf5", key=frame)
--- viz.py
+++ viz.py
@@ -1,77 +1,26 @@
 import h5py
-
-# from matplotlib import axes, figure
-# from pandas.core.frame import Axes
-from pandas.core.tools.datetimes import DatetimeScalar
 import polars as pl
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import numpy as np
-import openmeteo_requests
-import requests_cache
-from retry_requests import retry
+from pandas.core.tools.datetimes import DatetimeScalar
 from matplotlib.collections import LineCollection
 from datetime import datetime, timedelta
 
-"""
-cache_session = requests_cache.CachedSession(".cache", expire_after=-1)
-retry_session = retry(cache_session, retries=5, backoff_factor=0.2)
-openmeteo = openmeteo_requests.Client(session=retry_session)
-url = "https://archive-api.open-meteo.com/v1/archive"
-
-params = {
-    "latitude": 52.52,
-    "longitude": 13.41,
-    "start_date": "2024-12-27",
-    "end_date": "2025-01-10",
-    "daily": "temperature_2m_mean",
-    "temporal_resolution": "hourly_6",
-}
-
-responses = openmeteo.weather_api(url, params=params)
-response = responses[0]
-daily = response.Daily()
-daily_temperature_2m_mean = daily.Variables(0).ValuesAsNumpy()
-
-daily_data = {
-    "date": pd.date_range(
-        start=pd.to_datetime(daily.Time(), unit="s", utc=True),
-        end=pd.to_datetime(daily.TimeEnd(), unit="s", utc=True),
-        freq=pd.Timedelta(seconds=daily.Interval()),
-        inclusive="left",
-    )
-}
-
-daily_data["temperature_2m_mean"] = daily_temperature_2m_mean
-
-daily_dataframe = pd.DataFrame(data=daily_data)
-print(daily_dataframe)
-"""
 
 webpages = pd.read_csv("websites.csv")
-locations = webpages.get_column("Location")
+locations = webpages["Location"]
 data = h5py.File("weather.hdf5", "r")
 today = datetime.now().date()
-# """
-df = pl.DataFrame(
+df = pd.DataFrame(
     {
-        "date": [today - timedelta(x) for x in range(99)],
-        "locations": [
-            "Abuja",
-            "Bad Honeff",
-            "London",
-        ]
-        * 33,
+        "time": [today - timedelta(x) for x in range(99)],
         "Temperature": np.random.uniform(20, 40, 99),
         "Humidity": np.random.uniform(40, 70, 99),
         "Wind Speed": np.random.uniform(5, 15, 99),
     }
 )
-abuja = df.filter(pl.col("locations") == "Abuja")
-bh = df.filter(pl.col("locations") == "Bad Honeff")
-london = df.filter(pl.col("locations") == "London")
-# """
 
 
 fig, ax = plt.subplots(figsize=(8, 5))
@@ -90,17 +39,25 @@ def plot_vals(dfs, col, figname):
     for df in dfs:
         ax.xaxis.set_major_formatter(mdates.DateFormatter("%Y-%m-%d"))
         ax.xaxis.set_major_locator(mdates.DayLocator(interval=3))
-        ax.plot(df[:, 0].str.strptime(pl.Datetime, format="%Y-%m-%dT%H:%M"), df[:, col])
+        ax.plot(pd.to_datetime(df.iloc[:, 0], format="%Y-%m-%d"), df.iloc[:, col])
         ax.set_xlabel(f"{df.columns[col]}")
     fig.savefig(f"{figname}.png")
 
 
 def plot_loc(dfs, figname):
     fig, ax = plt.subplots()
+    ax.xaxis.set_major_formatter(mdates.DateFormatter("%Y-%m-%d"))
+    ax.xaxis.set_major_locator(mdates.DayLocator(interval=10))
     for df in dfs:
-        ax.xaxis.set_major_formatter(mdates.DateFormatter("%Y-%m-%d"))
-        ax.xaxis.set_major_locator(mdates.DayLocator(interval=2))
-        as.plot(df.iloc[:,0].str.strptime(format="%Y-%m-%dT%H:%M"))
+        times = pd.to_datetime(df.iloc[:, 0], format="%Y-%m-%d")
+        ax.plot(times, df.iloc[:, 1])
+        ax.plot(times, df.iloc[:, 2])
+        ax.plot(times, df.iloc[:, 3])
+        # ax.set_label(df.columns)
+        ax.set_xlabel(f"{df.columns[0]}")
+        ax.legend(df.columns[1:])
+    fig.savefig(f"{figname}")
 
 
-# plot_vals(dataframes, 1, "plt1")
+plot_vals(dataframes, 1, "plt1")
+plot_loc([df, df, df], "new.png")
